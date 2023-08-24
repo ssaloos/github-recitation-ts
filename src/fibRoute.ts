91e6cdc1ebd7fed10d4b45038a37166ebbc66ef6
mrahmatu <mrahmatu@andrew.cmu.edu>
2023-08-24 19:35:25 +0300

    nal change to fixed routes - 7:35 pm

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,18 @@
 import { fibonacci } from "./fib"; // Assuming the path to fib.ts is correct
+// we need this import to fix the error
 import { Request, Response } from "express";
 
 export default (req: Request, res: Response) => {
   const { num } = req.params;
   const parsedNum = parseInt(num, 10);
 
+  //if its a non-int then put in an error message
   if (isNaN(parsedNum)) {
     res.status(400).send("Invalid input: 'num' should be a valid number.");
     return;
   }
 
+  //calling on fibonacci
   const fibN = fibonacci(parsedNum);
   let result: string;
 
@@ -19,5 +22,6 @@ export default (req: Request, res: Response) => {
     result = `fibonacci(${parsedNum}) is ${fibN}`;
   }
 
+  //returns result
   res.send(result);
 };
