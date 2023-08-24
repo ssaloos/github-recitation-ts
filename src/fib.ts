ec8a3494c1707edf5e2b94c24f90247940bfaee2
Sara Al-Saloos <ssaloos@andrew.cmu.edu>
2023-08-24 19:21:37 +0300

    added comments and updated code

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,15 @@
 // util function that computes the fibonacci numbers
 export function fibonacci(n: number): number {
+  // if 0
   if (n < 0) {
     return -1;
+  // if is 0
   } else if (n === 0) {
     return 0;
+  // if is 1
   } else if (n === 1) {
     return 1;
   }
-
+  // the result to return
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
